4b03d3192c695ff949a8ddfd8b3e6246973c862a
juliendidier <julien@didier.io>
2013-08-03 19:00:31 +0200

    slide as fragment

--- SlidingLayerSample/src/com/slidinglayersample/MainActivity.java
+++ SlidingLayerSample/src/com/slidinglayersample/MainActivity.java
@@ -27,36 +27,22 @@ package com.slidinglayersample;
 
 import android.annotation.SuppressLint;
 import android.support.v4.app.FragmentActivity;
-import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.KeyEvent;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
-import android.widget.TextView;
-import com.slidinglayer.SlidingLayer;
 
-public class MainActivity extends FragmentActivity {
-
-    private SlidingLayer mSlidingLayer;
-    private TextView swipeText;
-
-    private String mStickContainerToRightLeftOrMiddle;
-    private boolean mShowShadow;
-    private boolean mShowOffset;
-
+public class MainActivity extends FragmentActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getPrefs();
-        bindViews();
-        initState();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        SlideFragment slide = new SlideFragment();
+        ft.add(R.id.detail_layout_wrapper, slide, "slide");
+        ft.commit();
     }
 
     @SuppressLint("NewApi")
@@ -67,72 +53,5 @@ public class MainActivity extends FragmentActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
-    }
-
-    /**
-     * View binding
-     */
-    private void bindViews() {
-        mSlidingLayer = (SlidingLayer) findViewById(R.id.slidingLayer1);
-        swipeText = (TextView) findViewById(R.id.swipeText);
-    }
-
-    /**
-     * Get current value for preferences
-     */
-    private void getPrefs() {
-        mStickContainerToRightLeftOrMiddle = "bottom";
-        mShowShadow = false;
-        mShowOffset = true;
-    }
-
-    /**
-     * Initializes the origin state of the layer
-     */
-    private void initState() {
-
-        // Sticks container to right or left
-        LayoutParams rlp = (LayoutParams) mSlidingLayer.getLayoutParams();
-        int textResource;
-        Drawable d;
-
-        textResource = R.string.swipe_down_label;
-        d = getResources().getDrawable(R.drawable.container_rocket);
-
-        mSlidingLayer.setStickTo(SlidingLayer.STICK_TO_BOTTOM);
-        rlp.width = LayoutParams.MATCH_PARENT;
-        rlp.height = getResources().getDimensionPixelSize(R.dimen.layer_width);
-
-        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
-        swipeText.setCompoundDrawables(null, d, null, null);
-        swipeText.setText(getResources().getString(textResource));
-        mSlidingLayer.setLayoutParams(rlp);
-
-        // shadow
-        mSlidingLayer.setShadowWidth(0);
-        mSlidingLayer.setShadowDrawable(null);
-
-        // offset
-        mSlidingLayer.setOffsetWidth(getResources().getDimensionPixelOffset(R.dimen.offset_width));
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-        case KeyEvent.KEYCODE_BACK:
-            if (mSlidingLayer.isOpened()) {
-                mSlidingLayer.closeLayer(true);
-                return true;
-            }
-
-        default:
-            return super.onKeyDown(keyCode, event);
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        finish();
-        return true;
     }
 }
---
+++ SlidingLayerSample/src/com/slidinglayersample/SlideFragment.java
@@ -0,0 +1,103 @@
+package com.slidinglayersample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+import com.slidinglayer.SlidingLayer;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.graphics.drawable.Drawable;
+
+public class SlideFragment extends Fragment
+{
+    private TextView swipeText;
+    private SlidingLayer mSlidingLayer;
+
+    private String mStickContainerToRightLeftOrMiddle;
+    private boolean mShowShadow;
+    private boolean mShowOffset;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.slide_view, container, false);
+
+        getPrefs(view);
+        bindViews(view);
+        initState(view);
+
+        return view;
+    }
+
+    /**
+     * View binding
+     */
+    private void bindViews(View view) {
+        mSlidingLayer = (SlidingLayer) view.findViewById(R.id.slidingLayer1);
+        swipeText = (TextView) view.findViewById(R.id.swipeText);
+    }
+
+    /**
+     * Get current value for preferences
+     */
+    private void getPrefs(View view) {
+        mStickContainerToRightLeftOrMiddle = "bottom";
+        mShowShadow = false;
+        mShowOffset = true;
+    }
+
+    /**
+     * Initializes the origin state of the layer
+     */
+    private void initState(View view) {
+
+        // Sticks container to right or left
+        LayoutParams rlp = (LayoutParams) mSlidingLayer.getLayoutParams();
+        int textResource;
+        Drawable d;
+
+        textResource = R.string.swipe_down_label;
+        d = getResources().getDrawable(R.drawable.container_rocket);
+
+        mSlidingLayer.setStickTo(SlidingLayer.STICK_TO_BOTTOM);
+        rlp.width = LayoutParams.MATCH_PARENT;
+        rlp.height = getResources().getDimensionPixelSize(R.dimen.layer_width);
+
+        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+        swipeText.setCompoundDrawables(null, d, null, null);
+        swipeText.setText(getResources().getString(textResource));
+        mSlidingLayer.setLayoutParams(rlp);
+
+        // shadow
+        mSlidingLayer.setShadowWidth(0);
+        mSlidingLayer.setShadowDrawable(null);
+
+        // offset
+        mSlidingLayer.setOffsetWidth(getResources().getDimensionPixelOffset(R.dimen.offset_width));
+    }
+
+    // @Override
+    // public boolean onKeyDown(int keyCode, KeyEvent event) {
+    //     switch (keyCode) {
+    //     case KeyEvent.KEYCODE_BACK:
+    //         if (mSlidingLayer.isOpened()) {
+    //             mSlidingLayer.closeLayer(true);
+    //             return true;
+    //         }
+
+    //     default:
+    //         return super.onKeyDown(keyCode, event);
+    //     }
+    // }
+
+    // @Override
+    // public boolean onOptionsItemSelected(MenuItem item) {
+    //     finish();
+    //     return true;
+    // }
+}
